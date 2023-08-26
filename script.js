0d51b5ba10f0f265e79df63fe0e3853588c71be9
Clayton Kennedy <clayton-kennedy@hotmail.com>
2023-08-26 11:49:41 -0300

    Pokedex

---
+++ script.js
@@ -0,0 +1,60 @@
+const pokemonName = document.querySelector('.nome-pokemon');
+const pokemonNumber = document.querySelector('.numero-pokemon');
+const pokemonImage = document.querySelector('.pokemon-img');
+
+const form = document.querySelector('.form');
+const input = document.querySelector('.buscar');
+const buttonPrev = document.querySelector('.btn-prev');
+const buttonNext = document.querySelector('.btn-next');
+
+let buscarpokemon = 1;
+
+const fetchPokemon = async (pokemon) => {
+     const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+
+     if (APIResponse.status == 200) {
+          const data = await APIResponse.json();
+          return(data);
+     }
+}
+
+const renderPokemon = async (pokemon) => {
+
+     pokemonName.innerHTML = 'Carregando...';
+     pokemonNumber.innerHTML = '';
+
+     const data = await fetchPokemon(pokemon);
+
+     if (data) {
+          pokemonName.innerHTML = data.name;
+          pokemonNumber.innerHTML = data.id;
+          pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default']; 
+          input.value = '';
+          buscarpokemon = data.id;
+     }
+     else {
+          pokemonImage.style.display = 'none';
+          pokemonName.innerHTML = 'Não encontrado';
+          pokemonNumber.innerHTML = '';
+     }
+}
+
+form.addEventListener('submit', (event) => {
+     event.preventDefault();
+     console.log('enviando formulário...')
+
+     renderPokemon(input.value.toLowerCase()); /*toLowerCase para transformar todos caracteres digitados em minusculo */
+     input.value = ''; /* para limpar a busca após a pesquisa */
+
+});
+buttonPrev.addEventListener('click', () => {
+     if (buscarpokemon > 1){
+     buscarpokemon -= 1;
+     renderPokemon(buscarpokemon);
+     }
+});
+buttonNext.addEventListener('click', () => {
+     buscarpokemon += 1;
+     renderPokemon(buscarpokemon);
+});
+renderPokemon('1');
